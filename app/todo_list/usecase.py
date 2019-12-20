32c499adce19e2f09094472f097cd4a8898e3d42
Dmytro Hordon <dmytro@preen.me>
2019-12-21 00:26:21 +0200

    removed extra query to db

--- app/todo_list/usecase.py
+++ app/todo_list/usecase.py
@@ -18,13 +18,10 @@ class TodoListUseCase:
         db.session.add(todo_list)
         try:
             db.session.commit()
-            # for now I don't know why. But a can't get todo_list->id property outside this try context
-            # so save id here
-            todo_list_id = todo_list.id
         except IntegrityError:
             self.error = {'title': f'Todo list with title "{title}" already exists.'}
         except DataError as e:
             logger.exception(e)
             self.error = {'todo-list': 'Something went wrong.'}
         else:
-            return TodoList.query.get(todo_list_id)
+            return todo_list
