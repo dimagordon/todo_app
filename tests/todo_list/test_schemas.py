e426bf88249b79f10881125f50307474701d0d84
Dmytro Hordon <dmytro@preen.me>
2020-01-10 13:27:47 +0200

    work mistype fixed

--- tests/todo_list/test_schemas.py
+++ tests/todo_list/test_schemas.py
@@ -1,7 +1,7 @@
 from app.api.v1.todo_list.schemas import todo_list_schema
 
 
-def test_unknow_field_exclude_schema():
+def test_unknonw_field_exclude_schema():
     required_title = {'title': 'some title'}
     unknown_fields_to_exclude = {'foo': 'bar', 'test': 'passed'}
     fields = dict(**required_title, **unknown_fields_to_exclude)
